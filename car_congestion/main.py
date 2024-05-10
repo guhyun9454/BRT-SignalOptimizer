ac0969a1e20e6aa243b2c7ce78380590e5b4f048
KHU D.com 인지 23 신지혜 <135792125+Jihye1005@users.noreply.github.com>
2024-05-11 00:56:23 +0900

    car_congestion

--- car_congestion/main.py
+++ car_congestion/main.py
@@ -4,32 +4,32 @@ import numpy as np
 from ROI import ROI
 from functions import draw_polygon
 
-# YOLO 모델 로드
+# YOLO model loading
 model = YOLO('yolo-Weights/yolov8m.pt')
 
-# 비디오 파일 경로
+# Video file path
 source = "videos/view3.mp4"
 
-# 대상 클래스의 인덱스
+# Target class indices
 class_indices = [2, 7, 5]
 
-# 비디오 캡처
+# Video capture
 cap = cv2.VideoCapture(source)
 
-# 교차보도 영역 좌표
+# Crosswalk area coordinates
 crosswalk = ROI((540, 166), (209, 836), (1499, 834), (890, 171))
 
 while True:
     success, img = cap.read()
     cmask = img.copy()
 
-    # 객체 수 초기화
+    # Initializing the number of objects
     current_count = 0
 
-    # 객체 검출
+    # Object detection
     results = model(img, stream=True, classes=class_indices, device="mps", conf=0.5)
 
-    # 객체 감지된 바운딩 박스 그리기 및 수 세기
+    # Drawing detected bounding boxes and counting objects
     for r in results:
         boxes = r.boxes
         for box in boxes:
@@ -37,19 +37,31 @@ while True:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
             
-            # 바운딩 박스 중심 좌표
+            # Calculating the center coordinates of the bounding box
             center_x = (x1 + x2) // 2
             center_y = (y1 + y2) // 2
             
-            # 교차보도 영역 내에 있는지 확인
+            # Checking if the bounding box is inside the crosswalk area
             if crosswalk.is_inside((center_x, center_y)):
                 current_count += 1
 
-        # 교차보도 영역 그리기
+    # Drawing the crosswalk area
     draw_polygon(img, crosswalk, (0, 0, 50), 0.5)
 
-    # 객체 수 출력
-    cv2.putText(img, f'Total Count: {current_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+   # Assigning message and color based on the number of objects
+    if current_count <= 1:
+        message = "Sparse"
+        color = (0, 255, 0)  # Green
+    elif current_count <= 4:
+        message = "Normal"
+        color = (0, 255, 255)  # Yellow
+    else:
+        message = "Congested"
+        color = (0, 0, 255)  # Red
+
+    # Displaying the number of objects and congestion level with color
+    cv2.putText(img, f'Total Count: {current_count}', (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255,255,255), 5)
+    cv2.putText(img, f'Congestion: {message}', (30, 160), cv2.FONT_HERSHEY_SIMPLEX, 2.5, color, 5)
 
     cv2.imshow('Object Detection Demo', img)
     if cv2.waitKey(1) == ord('q'):
